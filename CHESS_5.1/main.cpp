4a9c93ba687887b7f34b3eb2afab2f223af18a52
dekart9 <dekart9@yandex.ru>
2024-01-20 17:39:26 +0300

    2 цикла for при n*n

--- CHESS_5.1/main.cpp
+++ CHESS_5.1/main.cpp
@@ -1,92 +1,23 @@
 #include <iostream>
 using namespace std;
 
-//#define VERSION_1
-#define VERSION_2
 
 void main()
 {
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "Russian");
 
 	int n; // –азмер доски в клетках по ширине и высоте
-	cout << "Please input size of board: "; cin >> n;
+	cout << "¬ведите размер доски: "; cin >> n;
 	cout << endl;
 
-#if defined VERSION_1
-	for (int i = 0; i < n; i++)
+
+	for (int i = 0; i < n*n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < n*n; j++)
 		{
-			for (int k = 0; k < n; k++)
-			{
-				if (i % 2 == k % 2) for (int m = 0; m < n; m++) cout << "* ";
-
-				else for (int m = 0; m < n; m++) cout << "  ";
-			}
-			cout << endl;
-		}
-	}
-#endif
-
-#if defined VERSION_2
-	string BLACK_STRING;
-	string WHITE_STRING;
-	
-	switch (n)
-	{
-	case 2:
-		BLACK_STRING = "* *";
-		WHITE_STRING = "   ";
-		break;
-	case 3:
-		BLACK_STRING = "* * *";
-		WHITE_STRING = "     ";
-		break;
-	case 4:
-		BLACK_STRING = "* * * *";
-		WHITE_STRING = "       ";
-		break;
-	case 5:
-		BLACK_STRING = "* * * * *";
-		WHITE_STRING = "         ";
-		break;
-	case 6:
-		BLACK_STRING = "* * * * * *";
-		WHITE_STRING = "           ";
-		break;
-	case 7:
-		BLACK_STRING = "* * * * * * *";
-		WHITE_STRING = "             ";
-		break;
-	case 8:
-		BLACK_STRING = "* * * * * * * *";
-		WHITE_STRING = "               ";
-		break;
-	case 9:
-		BLACK_STRING = "* * * * * * * * *";
-		WHITE_STRING = "                 ";
-		break;
-	case 10:
-		BLACK_STRING = "* * * * * * * * * *";
-		WHITE_STRING = "                   ";
-		break;
-	default:
-		cout << "With this size of board we wonТt be able to do anything!";
-		break;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			for (int k = 0; k < n; k++)
-			{
-				if (i % 2 == k % 2) cout << BLACK_STRING;
-				else cout << WHITE_STRING;
-			}
-			cout << endl;
+				if ((i / n) % 2 == (j / n) % 2) cout << "* ";
+				else cout << "  ";
 		}
 		cout << endl;
 	}
-#endif
 }
